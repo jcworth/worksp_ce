e70a5b942434e4294353cb2f0c21cfcaba2a89bd
Jack Holdsworth <jackch90@gmail.com>
2020-03-17 17:22:56 +0000

    dead lines removed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -27,18 +27,4 @@ const initMapbox = () => {
   }
 };
 
-
-
 export { initMapbox };
-
-
-// const addMarkersToMap = (map, markers) => {
-//   markers.forEach((marker) => {
-//     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
-
-//     new mapboxgl.Marker()
-//       .setLngLat([ marker.lng, marker.lat ])
-//       .setPopup(popup) // add this
-//       .addTo(map);
-//   });
-// };
