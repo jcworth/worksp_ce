27187f73aa8e4c5a3144baa65f4599ad02fcdf5e
Jack Holdsworth <jackch90@gmail.com>
2020-03-17 18:41:43 +0000

    Added description field to Meeting, and basic styling to new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_134710) do
+ActiveRecord::Schema.define(version: 2020_03_17_184034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_134710) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
   end
 
   create_table "memberships", force: :cascade do |t|
