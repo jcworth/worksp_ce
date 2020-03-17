47d5defb058b2c12f04363c34d64a15f8000d0dc
Jack Holdsworth <jackch90@gmail.com>
2020-03-17 13:58:09 +0000

    Installed and configured geocoder for Meetings

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,6 @@
 class Meeting < ApplicationRecord
   has_many :attendees, dependent: :destroy
   has_many :users, through: :attendees
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
