9aaf0b4492bd3995400ec07f984826882008d3ed
Jack Holdsworth <jackch90@gmail.com>
2020-03-17 14:23:40 +0000

    Installed Mapbox, added homepage view of map

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @meetings = Meeting.geocoded
+    @markers = @meetings.map do |meeting|
+      {
+        lat: meeting.latitude,
+        long: meeting.longitude
+      }
+    end
   end
 end
