76f3c9838ca1ac8b104f5f598cb65c84782c8995
Jack Holdsworth <jackch90@gmail.com>
2020-03-20 10:13:34 +0000

    Generated user controller, view. and added name details to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_134710) do
+ActiveRecord::Schema.define(version: 2020_03_20_101236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_134710) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.text "description"
   end
 
   create_table "memberships", force: :cascade do |t|
@@ -56,6 +57,9 @@ ActiveRecord::Schema.define(version: 2020_03_17_134710) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
