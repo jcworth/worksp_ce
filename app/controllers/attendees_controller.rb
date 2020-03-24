fe2d4c215b748daf50c8a8b0495736ec314fb1c5
Jack Holdsworth <jackch90@gmail.com>
2020-03-24 14:49:55 +0000

    Profile card + meeting cards

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,12 +2,19 @@ class AttendeesController < ApplicationController
 
   def new
     @attendee = Attendee.new
-    @meeting = Meeting.find(params[:meeting_id])
+    # @meeting = Meeting.find(params[:meeting_id])
   end
 
   def create
-    @attendee = Attendee.find(params[:id])
+    @attendee = Attendee.new(validate_attendee)
+    meeting = Meeting.find(params[:meeting_id])
     @attendee.user = current_user
+    @attendee.meeting = meeting
+    if @attendee.save!
+      redirect_to meeting_path(meeting)
+    else
+      render :new
+    end
     # @attendee.confirmed = true
     # @Attendee.meeting = @meeting
   end
@@ -19,6 +26,10 @@ class AttendeesController < ApplicationController
   end
 
   private
+
+  def validate_attendee
+    params.require(:attendee).permit(:message)
+  end
 
 
 end
