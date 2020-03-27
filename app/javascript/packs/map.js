b0117ac14ca8d707ccc1b227a1aef59cc738562b
Jack Holdsworth <jackch90@gmail.com>
2020-03-27 14:19:23 +0000

    Removed maptype control

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -3,7 +3,8 @@ import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
 if (mapElement) {
-  const map = new GMaps({ div: '#map', lat: 0, lng: 0 });
+  const map = new GMaps({ div: '#map', lat: 0, lng: 0, mapTypeControl: false
+ });
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
