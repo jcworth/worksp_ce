38972e83975f15a69565c95ae538af89ac4237a2
Jack Holdsworth <jackch90@gmail.com>
2020-03-27 14:11:50 +0000

    Added info window partial

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,10 @@ class PagesController < ApplicationController
       data = Geocoder.search("#{meeting.google_place_id}", lookup: :google, google_place_id: true)
       {
         lat: data[0].geometry['location']['lat'],
-        lng: data[0].geometry['location']['lng']
+        lng: data[0].geometry['location']['lng'],
+        infoWindow: {
+          content: render_to_string(partial: "info_window", locals: { meeting: meeting })
+        }
         # infoWindow: render_to_string(partial: "info_window", locals: { meeting: meeting })
       }
     end
